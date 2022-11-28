4d411bcaaf08304f8534b5e7a248420c3364d117
JRookie <Ebaby1020@live.com>
2022-11-28 16:01:51 +0800

    init from mirai-console-plugin-template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.7.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.11.1"
+    id("net.mamoe.mirai-console") version "2.13.0-RC2"
 }
 
 group = "org.example"
@@ -13,3 +13,5 @@ repositories {
     maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
     mavenCentral()
 }
+
+
